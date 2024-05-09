99e0aab3847f58ed3b658474e1151cd594b786e6
Pedro Oliveira <p.duarteoliveiraa@gmail.com>
2024-05-09 14:47:12 -0300

    feat: moving pieces

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -9,7 +9,7 @@ public class ChessPosition {
 	
 	public ChessPosition(char column, Integer row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Erro ao instanciar ChessPosition. Os valores válidos são de a1 a h8.");
+			throw new ChessException("Erro ao instanciar posição de xadrez. Os valores válidos são de a1 a h8.");
 		}
 		this.column = column;
 		this.row = row;
