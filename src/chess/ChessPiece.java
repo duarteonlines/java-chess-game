f9aa27f002a73f790e271bcc86ce20c034ca849e
Pedro Oliveira <p.duarteoliveiraa@gmail.com>
2024-05-13 19:35:34 -0300

    feat: piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,14 +7,28 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
 	
 	private Color color;
+	private Integer moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
+		moveCount = 0;
 		this.color = color;
 	}
 
 	public Color getColor() {
 		return color;
+	}
+
+	public int getMoveCount() {
+		return this.moveCount;
+	}
+
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount--;
 	}
 
 	public ChessPosition getChessPosition() {
